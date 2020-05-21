3de81ea6f07b2af30ccc1cee02d17a7e7cc457fc
Dave Pernitz <pernitz@uwalumni.com>
2020-05-21 14:15:26 -0600

    Create docs folder

---
+++ docs/build/precache-manifest.ce780654dc51bb9c9d1e80a44fbb395c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e30a9adde844143f0920bdc5077692ce",
+    "url": "/whats-new/index.html"
+  },
+  {
+    "revision": "d1384bae604de5805b5b",
+    "url": "/whats-new/static/css/main.01b06aaa.chunk.css"
+  },
+  {
+    "revision": "18f1a80c8b819a0924bd",
+    "url": "/whats-new/static/js/2.69958e8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whats-new/static/js/2.69958e8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1384bae604de5805b5b",
+    "url": "/whats-new/static/js/main.b14e5c2c.chunk.js"
+  },
+  {
+    "revision": "deccfd15b8e90df9b470",
+    "url": "/whats-new/static/js/runtime-main.fe69efb1.js"
+  }
+]);
